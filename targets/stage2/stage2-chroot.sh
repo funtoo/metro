31d6ce2d42e193405c13c491a65f5f55b1f29be9
Daniel Robbins <drobbins@funtoo.org>
2008-08-27 16:42:50 -0600

    UTC default timezone and stage1 and 2 target cleanup

--- targets/stage2/stage2-chroot.sh
+++ targets/stage2/stage2-chroot.sh
@@ -9,17 +9,8 @@ setup_myfeatures
 # Setup the environment
 export FEATURES="${clst_myfeatures} nodoc noman noinfo"
 
-if [ "${clst_VERBOSE}" ]
-then
-	/usr/portage/scripts/bootstrap.sh -p ${bootstrap_opts}
-	echo "Press any key within 15 seconds to pause the build..."
-	read -s -t 15 -n 1
-	if [ $? -eq 0 ]
-	then
-		echo "Press any key to continue..."
-		read -s -n 1
-	fi
-fi
+echo "Here is what we are using for bootstrap:"
+/usr/portage/scripts/bootstrap.sh -p ${bootstrap_opts}
 
 ## START BUILD
 /usr/portage/scripts/bootstrap.sh ${bootstrap_opts} || exit 1
