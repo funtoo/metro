1ad164b975ded4e0dbf2726355221cfebed4d675
Daniel Robbins <drobbins@funtoo.org>
2009-03-03 15:41:17 -0700

    tightening security on Metro temp. directory tree

--- modules/targets.py
+++ modules/targets.py
@@ -92,8 +92,13 @@ class target:
 			print "Cleaning up %s..." % path
 		self.cmd(bin["rm"]+" -rf "+path)
 		if recreate:
-			self.cmd(bin["install"]+" -d "+path)
-
+			# This line ensures that the root /var/tmp/metro path has proper 0700 perms:
+			self.cmd(bin["install"]+" -d -m 0700 -g root -o root " + self.settings["path/tmp"])
+			# This creates the directory we want.
+			self.cmd(bin["install"]+" -d -m 0700 -g root -o root "+path)
+			# The 0700 perms prevent Metro-generated /tmp directories from being abused by others -
+			# because they are world-writeable, they could be used by malicious local users to 
+			# inject arbitrary data/executables into a Metro build.
 	def cmd(self,mycmd,myexc="",badval=None):
 		print "Executing \""+mycmd+"\"..."
 		#print "Executing \""+mycmd.split(" ")[0]+"\"..."
