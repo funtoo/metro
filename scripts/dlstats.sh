13c59fb2b392771b7578ca5ae9ec6f6c614afc15
Daniel Robbins <drobbins@funtoo.org>
2009-12-31 12:57:20 -0700

    misc changes without description as usual

--- scripts/dlstats.sh
+++ scripts/dlstats.sh
@@ -4,7 +4,7 @@ cat cherokee.access | tail -n 1 | cut -f4,5 -d" "
 
 # Set $build to the build you want stats on, or set to "" for all builds (total)
 
-build="funtoo"
+build=""
 
 for sub in i686 core2 amd64 x86 core2_32 pentium4 opteron athlon-xp phenom opteron_32
 do
