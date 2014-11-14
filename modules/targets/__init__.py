cf2ba2550a01388b4cd9fa2a84be18df25a2f5be
Daniel Robbins <drobbins@funtoo.org>
2014-11-14 10:29:40 -0700

    Buildrepo fixed for remote builds. Get mirror path from metro itself.

--- modules/targets/__init__.py
+++ modules/targets/__init__.py
@@ -2,9 +2,10 @@ from .snapshot import SnapshotTarget
 from .chroot import ChrootTarget
 from .stage import StageTarget
 from .virtualbox import VirtualboxTarget
+import sys
 try:
 	import boto.ec2
 	from .ec2 import Ec2Target
 except ImportError:
-	print("No boto available; ec2 support disabled.")
+	sys.stderr.write("No boto available; ec2 support disabled.\n")
 
