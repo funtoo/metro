b0e2dc410852f85da4a1e6b734e6cb171e177c4e
Daniel Robbins <drobbins@funtoo.org>
2015-10-20 22:57:08 -0600

    Move away from bind-mounting /proc and /dev and manually do /dev/shm mounts. Ubuntu has /dev/shm as a symlink. This gets metro working under Ubuntu again.

--- modules/targets/chroot.py
+++ modules/targets/chroot.py
@@ -12,7 +12,7 @@ class ChrootTarget(BaseTarget):
 		self.required_files.append("path/mirror/source")
 
 		# define general linux mount points
-		self.mounts = {"/proc": "/proc", "/dev" : "/dev"}
+		self.mounts = { }
 
 		if "target/class" not in self.settings:
 			return
@@ -109,6 +109,10 @@ class ChrootTarget(BaseTarget):
 
 	def bind(self):
 		""" Perform bind mounts """
+		os.system(self.cmds["mount"]+" none -t proc %s/proc" % self.settings["path/work"])
+		if not os.path.exists("%s/dev/shm" % self.settings["path/work"]):
+			os.makedirs("%s/dev/shm" % self.settings["path/work"])
+		os.system(self.cmds["mount"]+" none -t tmpfs -o mode=1777 %s/dev/shm" % self.settings["path/work"])
 		for dst, src in list(self.mounts.items()):
 			if not os.path.exists(src):
 				os.makedirs(src, 0o755)
@@ -121,6 +125,8 @@ class ChrootTarget(BaseTarget):
 			if os.system(self.cmds["mount"]+" -R "+src+" "+wdst) != 0:
 				self.unbind()
 				raise MetroError("Couldn't bind mount "+src)
+		self.mounts["/dev/shm"] = "/dev/shm"
+		self.mounts["/proc"] = "/proc"
 
 	def unbind(self, attempt=0):
 		mounts = self.get_active_mounts()
