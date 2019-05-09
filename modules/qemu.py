7dc7d790c94a8d7a413db22d988b08ba97d52502
Moritz Maxeiner <mm@ucw.sh>
2019-05-09 21:20:48 +0200

    riscv: Preliminary support

--- modules/qemu.py
+++ modules/qemu.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 qemu_arch_settings = {
+	'riscv-64bit': {
+		'qemu_binary': 'qemu-riscv64',
+		'qemu_cpu': 'sifive-u54',
+		'hexstring': [ '7f454c460201010000000000000000000200f3', '7f454c460201010000000000000000000300f3' ]
+	},
 	'arm-64bit': {
 		'qemu_binary': 'qemu-aarch64',
 		'qemu_cpu': 'cortex-a53',
@@ -14,6 +19,7 @@ qemu_arch_settings = {
 }
 
 native_support = {
+	'riscv': [],
 	'x86-64bit': ['x86-32bit'],
 	'x86-32bit': [],
 	'arm-64bit': ['arm-32bit'],
